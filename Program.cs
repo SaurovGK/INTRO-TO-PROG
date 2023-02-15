dfe4aff99ac035c20bf14bbbfc55500692d974a4
Saurov Gleb <saurov.gleb@gmail.com>
2023-02-15 21:49:10 +0300

    Добавлена консоль и Задача 2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.Write("Введите число 1: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 2: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x > y){
+    Console.WriteLine($"max = {x}");
+    Console.WriteLine($"min = {y}");
+}
+else{
+   Console.WriteLine($"max = {y}");
+   Console.WriteLine($"min = {x}");  
+}
+
