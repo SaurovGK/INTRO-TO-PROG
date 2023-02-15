765731048a2225997fc79b0fbf940b9e2185c9e2
Saurov Gleb <saurov.gleb@gmail.com>
2023-02-15 22:01:08 +0300

    Создана папка Задачи 4 и вставлена задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.Write("Введите число 1: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 2: ");
+int y = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 3: ");
+int z = int.Parse(Console.ReadLine()!);
+
+int max = x;
+
+if (x < y) {
+    max = y;
+}
+
+if (max < z) {
+    max = z;
+}
+
+Console.WriteLine($"Максимальное число = {max}");
