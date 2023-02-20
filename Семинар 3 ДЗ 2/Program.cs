103c57146e42e149b272f14ba433f4dec585e2bc
Saurov Gleb <saurov.gleb@gmail.com>
2023-02-20 14:06:24 +0300

    Задание 3 после 2 семинара

---
+++ Семинар 3 ДЗ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на ввод число (N) 
+// и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("введите число: ");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= a)
+{
+    Console.Write(Math.Pow(count,3));
+    if (count != a)
+    {
+      Console.Write(", ");  
+    }
+count++;
+}
