b72f41bd269053b087df1e7978ac6dbfe6954bf2
Saurov Gleb <saurov.gleb@gmail.com>
2023-03-05 15:49:59 +0300

    7 семинар

---
+++ Семинар 7 з47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+      for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1) ; j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 3;
+int col = 4;
+
+double[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
---
+++ Семинар 7 з51/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив.
+// Найдите сумму элементов, находящихся на главной диагонали (с
+// индексами (0,0); (1;1) и т.д.
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int LineDia(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            if (i==j)
+            {
+                sum += array[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+      for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1) ; j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array2D = GetArray(row, col, - 10, 10);
+int sum = LineDia(array2D);
+PrintArray(array2D);
+Console.WriteLine();
+Console.Write(sum);
